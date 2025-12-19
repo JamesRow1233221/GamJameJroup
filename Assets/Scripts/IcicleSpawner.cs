7caac2f0fe1d6b0ff765e67539c34cd4cc768db0
JamesRow1233221 <145841339+JamesRow1233221@users.noreply.github.com>
2025-12-19 20:13:42 +0000

    Main game idea almost done

--- Assets/Scripts/IcicleSpawner.cs
+++ Assets/Scripts/IcicleSpawner.cs
@@ -2,15 +2,32 @@ using UnityEngine;
 
 public class IcicleSpawner : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private float _maxTime = 1.5f;
+    [SerializeField] private float _heightRange = 0.45f;
+    [SerializeField] private GameObject _icicle;
+
+        private float _timer;
+
+    private void Start()
     {
-        
+        SpawnIcicle();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (_timer > _maxTime)
+        {
+            SpawnIcicle();
+            _timer = 0;
+        }
+
+        _timer += Time.deltaTime;
+    }
+    private void SpawnIcicle()
+    {
+        Vector3 spawnPos = transform.position + new Vector3(0, Random.Range(-_heightRange, _heightRange));
+        GameObject icicle = Instantiate(_icicle, spawnPos, Quaternion.identity);
+
+        Destroy(icicle, 10f);
     }
 }
